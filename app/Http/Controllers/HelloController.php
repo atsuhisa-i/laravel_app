777abcefd1bcee8492e221bd566df96f4b6a87ca
atsuhisa-i <atsuhisa.1124@gmail.com>
2020-12-13 12:50:12 +0900

    リクエストとレスポンス7

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -21,7 +21,7 @@ class HelloController extends Controller
         $name = $request->query('name');
         $mail = $request->query('mail');
         $tel = $request->query('tel');
-        $msg = $name . ', ' . $mail . ', ' . $tel;
+        $msg = $request->query('msg');
         $keys = ['名前', 'メール', '電話'];
         $values = [$name, $mail, $tel];
         $data =[
@@ -33,11 +33,15 @@ class HelloController extends Controller
         return view('hello.index', $data);
     }
 
-    public function other(Request $request)
+    public function other()
     {
-        $ext = '.' . $request->file('file')->extension();
-        Storage::disk('public')->
-            putFileAs('files', $request->file('file'), 'uploaded' . $ext);
-        return redirect()->route('hello');
+        $data = [
+            'name' => 'Taro',
+            'mail' => 'taro@yamada',
+            'tel' => '090-999-999',
+        ];
+        $query_str = http_build_query($data);
+        $data['msg'] = $query_str;
+        return redirect()->route('hello', $data);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ Route::namespace('App\Http\Controllers\Sample')->group(function(){
     Route::get('/sample/other', 'SampleController@other');
 });
 
-Route::get('/hello', 'App\Http\Controllers\HelloController@index');
+Route::get('/hello', 'App\Http\Controllers\HelloController@index')->name('hello');
 Route::post('/hello', 'App\Http\Controllers\HelloController@index');
 Route::get('/hello/other', 'App\Http\Controllers\HelloController@other');
 Route::post('/hello/other', 'App\Http\Controllers\HelloController@other');
